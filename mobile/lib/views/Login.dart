e92d2fc89f796c71d2131d65c02906951b073d45
Paweł Budnik <82655@student.pb.edu.pl>
2024-03-18 20:24:27 +0100

    [PZGRAP-144] Nowy wygląd ekranu logowania

--- mobile/lib/views/Login.dart
+++ mobile/lib/views/Login.dart
@@ -1,12 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
 import 'package:mobile/viewModels/LoginViewModel.dart';
 import 'Menu.dart';
 import 'Register.dart';
+import 'package:animate_do/animate_do.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key});
 
   @override
   LoginPageState createState() => LoginPageState();
@@ -36,74 +39,153 @@ class LoginPageState extends State<LoginPage> with RouteAware {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Mafia+'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/mafialogo.png',
-                width: screenWidth * 0.65,
-            ),
-            const SizedBox(height: 16.0),
-            const Text(
-              'Login', // Napis po angielsku pod obrazkiem
-              style: TextStyle(
-                fontSize: 36.0, // Dostosuj rozmiar czcionki według potrzeb
+        body: Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  colors: [
+                    Color(0xFF8E44AD),
+                    Color(0xFFc8a2d8)
+                  ]
+              )
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(height: 40,),
+              Center(
+                child: Image.asset(
+                  'assets/images/mafialogo.png',
+                  width: screenWidth * 0.40,
+                ),
               ),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: _loginController,
-              decoration: const InputDecoration(labelText: 'Login'),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(labelText: 'Password'),
-            ),
-            const SizedBox(height: 32.0),
-            Consumer<LoginViewModel>(
-              builder: (context, viewModel, child) {
-                return ElevatedButton(
-                  onPressed: () async {
-                    bool isLogged = await viewModel.login(
-                      _loginController.text,
-                      _passwordController.text
-                    );
-                    if(isLogged) {
-                      Fluttertoast.showToast(
-                          msg: "Successful login",
-                          toastLength: Toast.LENGTH_SHORT
-                      );
-                      if(!context.mounted) return;
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const MenuPage())
-                      );
-                    }
-                  },
-                  child: const Text('Login')
-                );
-              }
-            ),
-            const SizedBox(height: 16.0),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const RegisterPage()),
-                );
-              },
-              child: const Text('Register'),
-            ),
-          ],
-        ),
-      ),
+              const Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("Mafia+",style: TextStyle(color: Colors.white, fontSize: 35),),
+                      Text("Login",style: TextStyle(color: Colors.white, fontSize: 20),)
+                    ]
+                ),
+              ),
+              Expanded(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(40), topRight: Radius.circular(40))
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(30),
+                      child: Column(
+                        children: <Widget>[
+                          const SizedBox(height: 15,),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: const [BoxShadow(
+                                color: Color(0x6D8E44AD),
+                                blurRadius: 20,
+                                offset: Offset(0, 10)
+                              )]
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  padding: const EdgeInsets.all(10),
+                                  decoration: const BoxDecoration(
+                                    border: Border(bottom: BorderSide(color: Colors.grey))
+                                  ),
+                                  child: TextField(
+                                    controller: _loginController,
+                                    decoration: const InputDecoration(
+                                      hintText: "Login",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(10),
+                                  child: TextField(
+                                    controller: _passwordController,
+                                    obscureText: true,
+                                    decoration: const InputDecoration(
+                                      hintText: "Password",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            )
+                          ),
+                          const SizedBox(height: 35,),
+                          Consumer<LoginViewModel>(
+                              builder: (context, viewModel, child) {
+                                return ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: const Color(0x6D8E44AD),
+                                      minimumSize: Size(screenWidth-150, 50)
+                                    ),
+                                    onPressed: () async {
+                                      bool isLogged = await viewModel.login(
+                                          _loginController.text,
+                                          _passwordController.text
+                                      );
+                                      if(isLogged) {
+                                        Fluttertoast.showToast(
+                                            msg: "Successful login",
+                                            toastLength: Toast.LENGTH_SHORT
+                                        );
+                                        if(!context.mounted) return;
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(builder: (context) => const MenuPage())
+                                        );
+                                      }
+                                    },
+                                    child: const Text(
+                                        'Login',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold
+                                      ))
+                                );
+                              }
+                          ),
+                          const SizedBox(height: 25.0),
+                          const Text("Don't have an account?"),
+                          const SizedBox(height: 8.0),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0x6D8E44AD),
+                                minimumSize: Size(screenWidth-150, 50)
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const RegisterPage()),
+                              );
+                            },
+                            child: const Text(
+                                'Register',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold
+                                ))
+                          ),
+                        ],
+                      ),
+                    ),
+                  ))
+            ],
+          ),
+        )
     );
   }
 }
