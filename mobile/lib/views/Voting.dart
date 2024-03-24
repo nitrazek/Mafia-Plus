4158b942531092560ab17d1ce4a83bfdc7db0d26
xShyQx <93669491+xShyQx@users.noreply.github.com>
2024-03-24 18:34:57 +0100

    [PZGRAP-158] Dodalem metode dispose do VotingPage aby usuwac nasluchiwanie na Stream z VotingViewModel

--- mobile/lib/views/Voting.dart
+++ mobile/lib/views/Voting.dart
@@ -32,6 +32,12 @@ class _VotingPageState extends State<VotingPage> {
   }
 
   @override
+  void dispose() {
+    _votingFinishedSubscription?.cancel();
+    super.dispose();
+  }
+  
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
