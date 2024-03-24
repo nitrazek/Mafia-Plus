d8236cced84082f5ed7da0a46dea57b882ecb68a
Paweł Budnik <82655@student.pb.edu.pl>
2024-03-24 14:56:58 +0100

    [PZGRAP-146] Dodanie tła i napisu

--- mobile/lib/views/RoomSettings.dart
+++ mobile/lib/views/RoomSettings.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import 'package:mobile/models/RoomSettings.dart';
 import 'package:mobile/viewModels/RoomSettingsViewModel.dart';
+import 'styles.dart';
 
 class RoomSettingsPage extends StatefulWidget {
   @override
@@ -16,79 +17,30 @@ class _RoomSettingsPageState extends State<RoomSettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Room Settings'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: Container(
+        width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              colors: [
+                MyStyles.appBarColor,
+                MyStyles.lightestPurple
+              ]
+            )
+          ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Room Type:',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Row(
-              children: [
-                Radio(
-                  value: true,
-                  groupValue: _isPublic,
-                  onChanged: (value) {
-                    setState(() {
-                      _isPublic = value!;
-                    });
-                  },
-                ),
-                Text('Public'),
-                Radio(
-                  value: false,
-                  groupValue: _isPublic,
-                  onChanged: (value) {
-                    setState(() {
-                      _isPublic = value!;
-                    });
-                  },
-                ),
-                Text('Private'),
+          children: <Widget>[
+            const SizedBox(height: 40,),
+            const Padding(
+                padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text("Room settings",style: TextStyle(color: Colors.white, fontSize: 30),),
               ],
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Number of Players:',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Slider(
-              value: _numberOfPlayers.toDouble(),
-              min: 4,
-              max: 10,
-              divisions: 6,
-              label: _numberOfPlayers.toString(),
-              onChanged: (value) {
-                setState(() {
-                  _numberOfPlayers = value.round();
-                });
-              },
-            ),
-            Text('$_numberOfPlayers Players'),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () async {
-                RoomSettings roomSettings = RoomSettings(
-                  isPublic: _isPublic,
-                  numberOfPlayers: _numberOfPlayers,
-                );
-                await context.read<RoomSettingsViewModel>().saveGameSettings(
-                    roomSettings);
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(double.infinity, 60),
-                textStyle: TextStyle(fontSize: 32.0),
-              ),
-              child: Text('Save'),
-            ),
-
+            ),)
           ],
-
         ),
       ),
     );
