b80cc084244fd76def9d9b5bed459d31822826ff
Mariusz Aniszczuk <80250@student.pb.edu.pl>
2024-03-24 16:50:13 +0100

    [PZGRAP-149] Ui do UserRole

--- mobile/lib/views/UserRole.dart
+++ mobile/lib/views/UserRole.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/Views/Menu.dart';
+import 'package:mobile/Views/styles.dart';
 import 'package:mobile/views/Voting.dart';
+import 'package:mobile/views/VotingResults.dart';
 import 'package:provider/provider.dart';
 import 'package:mobile/viewModels/WinnerRoleViewModel.dart';
 import 'package:mobile/models/Room.dart';
@@ -16,32 +19,80 @@ class _UserRolePageState extends State<UserRolePage> {
   void initState() {
     super.initState();
     // Start a timer to automatically navigate to the voting page after 3 seconds
-    Future.delayed(Duration(seconds: 3), () {
-      if (!buttonPressed) {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => VotingPage()),
-        );
-      }
-    });
+    // Future.delayed(Duration(seconds: 3), () {
+    //   if (!buttonPressed) {
+    //     Navigator.push(
+    //       context,
+    //       MaterialPageRoute(builder: (context) => VotingPage()),
+    //     );
+    //   }
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
+    String role = context.watch<WinnerRoleViewModel>().userRole;
+    // String role = 'mafia';
+    Color textColor = role == 'mafia' ? Colors.red : Colors.green;
+
     return Scaffold(
-      body: Center(
+      backgroundColor: MyStyles.backgroundColor,
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            colors: [MyStyles.purple, MyStyles.lightestPurple],
+          ),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Consumer<WinnerRoleViewModel>(
-              builder: (context, winnerRoleViewModel, child) {
-                return Text(
-                  'Your role is: '
-                      '${context.watch<WinnerRoleViewModel>().userRole}',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                );
-              },
-            )
+            const SizedBox(height: 100),
+            Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Your role is: $role',
+                        style: TextStyle(color: textColor, fontSize: 24),
+                      ),
+                      role == 'mafia'
+                          ? Image.asset(
+                              'assets/images/mafia.png',
+                              width: 300,
+                            )
+                          : Image.asset(
+                              'assets/images/citizen.png',
+                              width: 300,
+                            ),
+                      ElevatedButton(
+                        style: MyStyles.buttonStyle,
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => VotingResultsPage()),
+                          );
+                        },
+                        child: Text(
+                          "Voting",
+                          style: MyStyles.buttonTextStyle,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
