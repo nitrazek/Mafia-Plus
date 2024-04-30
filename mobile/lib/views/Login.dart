973c956445d5173f53db2342e9b997ceafde603f
Michal <officialdeps@gmail.com>
2024-04-30 17:11:49 +0200

    [PZGRAP] Dodanie kółeczka w oczekiwaniu na serwer

--- mobile/lib/views/Login.dart
+++ mobile/lib/views/Login.dart
@@ -22,6 +22,17 @@ class LoginPageState extends State<LoginPage> with RouteAware {
   late double screenWidth;
   late double screenHeight;
 
+  bool _isLoading = false;
+  void _getIsLoadingValue(BuildContext context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => RegisterPage()),
+    );
+    setState(() {
+      _isLoading = result;
+    });
+    }
+
   @override
   void initState() {
     super.initState();
@@ -139,7 +150,10 @@ class LoginPageState extends State<LoginPage> with RouteAware {
                               builder: (context, viewModel, child) {
                                 return ElevatedButton(
                                     style: MyStyles.buttonStyle,
-                                    onPressed: () async {
+                                    onPressed: _isLoading ? null : () async {
+                                      setState(() {
+                                        _isLoading = true;
+                                      });
                                       bool isLogged = await viewModel.login(
                                           _loginController.text,
                                           _passwordController.text
@@ -155,8 +169,13 @@ class LoginPageState extends State<LoginPage> with RouteAware {
                                             MaterialPageRoute(builder: (context) => const MenuPage())
                                         );
                                       }
+                                      setState(() {
+                                        _isLoading = false;
+                                      });
                                     },
-                                    child: const Text(
+                                    child: _isLoading
+                                        ? CircularProgressIndicator()
+                                        :Text(
                                         'Login',
                                       style: TextStyle(
                                         color: Colors.white,
@@ -171,13 +190,8 @@ class LoginPageState extends State<LoginPage> with RouteAware {
                           const SizedBox(height: 8.0),
                           ElevatedButton(
                             style: MyStyles.buttonStyle,
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => const RegisterPage()),
-                              );
-                            },
-                            child: const Text(
+                            onPressed: _isLoading ? null : () => _getIsLoadingValue(context),
+                            child:Text(
                                 'Register',
                                 style: TextStyle(
                                     color: Colors.white,
